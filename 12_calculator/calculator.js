d511f5c1e5863eb7c4bdc150ed388ceabb933ec8
Hardian E.C <dakochan64@gmail.com>
2025-10-29 09:39:41 +0700

    Solved javascript exercise for calculator app

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,47 @@
-const add = function() {
-	
+const add = function (number1, number2) {
+  return number1 + number2
 };
 
-const subtract = function() {
-	
+
+const subtract = function (number1, number2) {
+  return number1 - number2
 };
 
-const sum = function() {
-	
+
+const sum = function (arr) {
+  if (arr.length === 0) {
+    return 0;
+  }
+
+  if (arr.length > 1) {
+    return arr.reduce((acc, cv) =>
+      acc += cv
+      )
+  }else {
+    return arr[0]
+  }
 };
 
-const multiply = function() {
-
+const multiply = function (arr) {
+  return arr.reduce((acc, curval) => acc * curval)
 };
 
-const power = function() {
-	
+const power = function (number1, number2) {
+  return number1 ** number2
 };
 
-const factorial = function() {
-	
+
+const factorial = function (number) { 
+  if(number == 0){
+    return 1;
+  }else{
+    let ini = number;
+    for(let x = number; x > 1; x--){
+      ini = ini * (x - 1)
+    }
+      return ini
+
+  }
 };
 
 // Do not edit below this line
